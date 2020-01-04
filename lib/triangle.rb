c85ad91aef4774c24fc014b671ed0253c3ec0766
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-04 23:08:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,8 +1,14 @@
 class Triangle
   
-  attr_accessor :A, :B, :C
+  attr_accessor :A, :B, :C, :equilateral, :isosceles, :scalene
   
-  
+  def initialize(A, B, C)
+    :A = A 
+    :B = B
+    :C = C
+  end
   
   
 end
+
+class TriangleError < StandardError
