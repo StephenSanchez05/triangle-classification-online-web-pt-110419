e26bdfee2a24a2c9fbb87bb02c2d4a86d53d382e
Stephen Sanchez <stephensanchez05@gmail.com>
2020-01-04 23:38:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,7 @@ class Triangle
   
   def kind
     if @lengthA != 0 && @lengthA + @lengthB > @lengthC && @lengthB + @lengthC > @lengthA && @lengthA + @lengthC > @lengthB
-      type
+      puts type
     else
       begin
       raise TriangleError
